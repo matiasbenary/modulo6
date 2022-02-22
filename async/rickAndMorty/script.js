9b059106b07988fd46b2a863149032b3dc1b68e9
Matias Benary <matiasbenary@gmail.com>
2022-02-22 20:59:19 -0300

    :pencil: Clase 22/02

---
+++ async/rickAndMorty/script.js
@@ -0,0 +1,52 @@
+const cards = document.getElementById('cards');
+
+// const renderCards = () =>{
+//   fetch('https://rickandmortyapi.com/api/character/')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const infoCards = data.results;
+//     console.log(infoCards);
+//     let aux = '';
+//     for (const info of infoCards) {
+//       aux += `
+//     <div class="col">
+//       <div class="card">
+//         <img src="${info.image}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${info.name}</h5>
+//           <p class="card-text">${info.species}</p>
+//           <p class="card-text">${info.created}</p>
+//           <p class="card-text">Ubicacion ${info.location.name}</p>
+//         </div>
+//       </div>
+//     </div>`;
+//     }
+//     cards.innerHTML = aux;
+//   });
+// }
+
+const renderCards = async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/character/');
+  const data = await response.json();
+
+  const infoCards = data.results;
+  console.log(infoCards);
+  let aux = '';
+  for (const info of infoCards) {
+    aux += `
+    <div class="col">
+      <div class="card">
+        <img src="${info.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${info.name}</h5>
+          <p class="card-text">${info.species} - ${info.gender}</p>
+          <p class="card-text">${info.created}</p>
+          <p class="card-text">Ubicacion ${info.location.name}</p>
+        </div>
+      </div>
+    </div>`;
+  }
+  cards.innerHTML = aux;
+};
+
+renderCards();
--- ejercicios/ejercicio Zorro/resolucion/app.js
+++ ejercicios/ejercicio Zorro/resolucion/app.js
@@ -1,12 +1,12 @@
-const img = document.getElementById("img");
-const button = document.getElementById("btn");
+const img = document.getElementById('img');
+const button = document.getElementById('btn');
 
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
   addImg();
 });
 
 const addImg = () => {
-  fetch("https://randomfox.ca/floof/")
+  fetch('https://randomfox.ca/floof/')
     .then((res) => res.json())
     .then((response) => (img.src = response.image));
 };
